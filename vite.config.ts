94f97631080a6d9ab6014476942ac2f67cbb9fd8
Daniel Misic <daniel@misic.ca>
2025-11-26 09:47:38 -0500

    fix: update vite base url to match repo name cleanswift2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/cleanswift/',   // <-- REQUIRED FOR GITHUB PAGES
+      base: '/cleanswift2/',   // <-- REQUIRED FOR GITHUB PAGES
       server: {
         port: 3000,
         host: '0.0.0.0',
