c0315da3a7a87bfc884dbae93d80b868fbce581e
sixoverme <myvelouria777@gmail.com>
2026-05-07 13:05:57 -0400

    Revert "Fix Netlify White Page and SPA Routing"

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: process.env.NETLIFY ? '/' : '/cleanswift2/',
+      base: '/cleanswift2/',   // <-- REQUIRED FOR GITHUB PAGES
       server: {
         port: 3000,
         host: '0.0.0.0',
